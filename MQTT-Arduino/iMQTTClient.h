38d3fbdb1d9ed29b8d8f3e69083356a5ad412ce0
Timo <timodebimo@hotmail.com>
2018-04-10 13:52:42 +0200

    Even more MQTT for arduino

--- MQTT-Arduino/iMQTTClient.h
+++ MQTT-Arduino/iMQTTClient.h
@@ -1,12 +1,14 @@
 #ifndef IMQTTSERVER_H
 #define IMQTTSERVER_H
 
+#include <string>
+
 class iMQTTServer
 {
 public:
 	~iMQTTServer();
-	void SendInstruction();
-	void Reply();
+	void SendMessage(std::string message);
+	std::string ReceiveMessage();
 };
 
 #endif
