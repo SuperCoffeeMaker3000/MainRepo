6e43eea7a4c4e3f4921d1ec7d2d178204f42a17f
Unknown <bart.van.grimbergen@gmail.com>
2018-05-15 15:39:42 +0200

    Communicator

---
+++ Z_Communicator/Communicator.h
@@ -0,0 +1,23 @@
+#ifndef COMMUNICATOR_H
+#define COMMUNICATOR_H
+
+#include <SPI.h>
+#include <MCP2515.h>
+#include <stdint.h>
+
+
+class Communicator
+{
+public:
+	Communicator(unsigned long adrsValue);
+	~Communicator();
+	CANMSG msg;
+	MCP2515 can;
+	void SendCommand(uint8_t byteOne, uint8_t byteTwo, uint8_t byteThree, uint8_t byteFour, uint8_t byteFive);
+	void SendCommand(uint8_t byteOne, uint8_t byteTwo, uint8_t byteThree, uint8_t byteFour);
+	void SendCommand(uint8_t byteOne, uint8_t byteTwo, uint8_t byteThree);
+	void SendCommand(uint8_t byteOne, uint8_t byteTwo);
+
+};
+
+#endif
