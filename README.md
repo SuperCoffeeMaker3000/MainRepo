798146b8cdeea9beaecfe60e60602f6492b09324
Mark <marksteenmans@gmail.com>
2018-05-15 21:21:57 +0200

    Commit of Arduino.ino for the water module
    This arduino program will be the water module

--- README.md
+++
@@ -1,2 +0,0 @@
-# MainRepo
-Super Coffee Machine
---
+++ WaterModule/WaterModule.ino
@@ -0,0 +1,22 @@
+#include <Communicator.h>
+
+#define SensorPin A0
+#include <math.h>
+
+double GetTemp()
+{
+  double sensorvalue = analogRead(A0);
+  double voltage = analogRead(A0) / 204.8;
+  double temp = 12.657 * pow(voltage, 2) - 71.883 * voltage + 120.42;
+  return temp;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(GetTemp());
+  delay(100);
+}
