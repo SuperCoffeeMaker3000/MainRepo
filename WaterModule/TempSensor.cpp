339a52b4899fbbefb83a8704d06ab14d140bb1b6
Mark <marksteenmans@gmail.com>
2018-06-05 23:34:52 +0200

    Updated tempsensor temp. calculation

--- WaterModule/TempSensor.cpp
+++ WaterModule/TempSensor.cpp
@@ -4,11 +4,8 @@
 double TempSensor::GetData()
 {
   double sensorvalue = analogRead(SensorPin);
-
-  double voltage = analogRead(SensorPin) / 204.8; //voltage from arduino analogRead() goes from 0 to 1023. Voltage actually goes from 0 to 5. So divide by 204.8
-
-  double temp = 11.862 * pow(voltage, 2) - 69.875 * voltage + 121.01;
-
+  double voltage = sensorvalue / 204.8; //voltage from arduino analogRead() goes from 0 to 1023. Voltage actually goes from 0 to 5. So divide by 204.8
+  double temp = 12.449 * pow(voltage, 2) - 71.362 * voltage + 120.57;
   return temp;
 }
 
